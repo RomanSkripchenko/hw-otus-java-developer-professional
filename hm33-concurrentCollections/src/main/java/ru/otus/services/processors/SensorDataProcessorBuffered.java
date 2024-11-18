477b4697c3d7bf1b5fb58545480c52e310e0f418
Скрипченко Роман <skro@raytec.pro>
2024-11-18 16:22:38 +0300

    hm-33

---
+++ hm33-concurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -0,0 +1,52 @@
+package ru.otus.services.processors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.otus.api.SensorDataProcessor;
+import ru.otus.api.model.SensorData;
+import ru.otus.lib.SensorDataBufferedWriter;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class SensorDataProcessorBuffered implements SensorDataProcessor {
+    private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
+
+    private final int bufferSize;
+    private final SensorDataBufferedWriter writer;
+    private final List<SensorData> buffer;
+
+    public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
+        this.bufferSize = bufferSize;
+        this.writer = writer;
+        this.buffer = new ArrayList<>();
+    }
+
+    @Override
+    public synchronized void process(SensorData data) {
+        buffer.add(data);
+        if (buffer.size() >= bufferSize) {
+            flush();
+        }
+    }
+
+    @Override
+    public synchronized void onProcessingEnd() {
+        flush();
+    }
+
+    public synchronized void flush() {
+        if (buffer.isEmpty()) {
+            return;
+        }
+
+        try {
+            buffer.sort(Comparator.comparing(SensorData::getMeasurementTime));
+            writer.writeBufferedData(new ArrayList<>(buffer));
+            buffer.clear();
+        } catch (Exception e) {
+            log.error("Ошибка в процессе записи буфера", e);
+        }
+    }
+}
