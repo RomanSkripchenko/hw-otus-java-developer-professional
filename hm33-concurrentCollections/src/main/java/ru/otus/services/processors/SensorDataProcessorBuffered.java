05c795538093c5949f30f14b4ec6270a51896519
Скрипченко Роман <skro@raytec.pro>
2024-11-20 11:11:31 +0300

    PriorityBlockingQueue контейнер

--- hm33-concurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
+++ hm33-concurrentCollections/src/main/java/ru/otus/services/processors/SensorDataProcessorBuffered.java
@@ -1,5 +1,4 @@
 package ru.otus.services.processors;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.otus.api.SensorDataProcessor;
@@ -8,23 +7,24 @@ import ru.otus.lib.SensorDataBufferedWriter;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class SensorDataProcessorBuffered implements SensorDataProcessor {
     private static final Logger log = LoggerFactory.getLogger(SensorDataProcessorBuffered.class);
 
     private final int bufferSize;
     private final SensorDataBufferedWriter writer;
-    private final List<SensorData> buffer;
+    private final PriorityBlockingQueue<SensorData> buffer;
 
     public SensorDataProcessorBuffered(int bufferSize, SensorDataBufferedWriter writer) {
         this.bufferSize = bufferSize;
         this.writer = writer;
-        this.buffer = new ArrayList<>();
+        this.buffer = new PriorityBlockingQueue<>(bufferSize,
+                Comparator.comparing(SensorData::getMeasurementTime));
     }
 
     @Override
-    public synchronized void process(SensorData data) {
+    public void process(SensorData data) {
         buffer.add(data);
         if (buffer.size() >= bufferSize) {
             flush();
@@ -32,22 +32,22 @@ public class SensorDataProcessorBuffered implements SensorDataProcessor {
     }
 
     @Override
-    public synchronized void onProcessingEnd() {
+    public void onProcessingEnd() {
         flush();
     }
 
-    protected synchronized void flush() {
+    protected void flush() {
         if (buffer.isEmpty()) {
             return;
         }
 
         try {
-            List<SensorData> sortedBuffer = new ArrayList<>(buffer);
-            sortedBuffer.sort(Comparator.comparing(SensorData::getMeasurementTime));
-            writer.writeBufferedData(sortedBuffer);
-            buffer.clear();
+            var flushedData = new ArrayList<SensorData>();
+            buffer.drainTo(flushedData);
+            writer.writeBufferedData(flushedData);
         } catch (Exception e) {
             log.error("Ошибка в процессе записи буфера", e);
         }
     }
 }
+
